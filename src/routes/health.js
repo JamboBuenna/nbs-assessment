26500e0ce551be5f95897c5f684b6b856f80b24f
James Nurse <james.nurse@ag-space.com>
2021-11-19 19:41:25 +0000

    Test prettier hooks

--- src/routes/health.js
+++ src/routes/health.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('REST service working normally');
+          res.send('REST service working normally');
 });
 
 module.exports = router;
