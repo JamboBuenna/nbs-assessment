5754ac5c0bc872bacb09ed8bc87b1f9541d8728a
James Nurse <james.nurse@ag-space.com>
2021-11-19 19:55:23 +0000

    Test prettier hooks - 2

--- src/routes/health.js
+++ src/routes/health.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const router = express.Router();
 
+
+var a = "b" // this will yield eslint error
 router.get('/', (req, res, next) => {
-          res.send('REST service working normally');
+  res.send('REST service working normally');
 });
 
 module.exports = router;
