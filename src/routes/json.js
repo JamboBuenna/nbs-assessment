a379ce7b8f0076069ef0030d0630b6fdef2cfb6c
James Nurse <james.nurse@ag-space.com>
2021-11-20 11:58:33 +0000

    Revert mocha simplification.

--- src/routes/json.js
+++ src/routes/json.js
@@ -2,8 +2,74 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req, res, next) => {
-  console.log(req.body); // your JSON
-  res.send(req.body); // echo the result back
+  const inputArray = req.body;
+
+  const reorderedJSONArray = inputArray.map(function(obj){
+    return stringifySort(obj)
+  });
+
+  const uniqueArray = Array.from(new Set(reorderedJSONArray));
+  const inputLength = inputArray.length;
+  const uniqueLength = uniqueArray.length
+
+  const response = {
+    inputLength,
+    uniqueLength,
+    repeatedItems: inputLength - uniqueLength
+  }
+
+  res.send(response);
 });
+
+/**
+ * An implementation of sort that can handle arrays &
+ * //TODO - Work out what's going on here.
+ * @param unordered The unordered JSON object
+ * @param sortArrays: boolean Whether or not to reorder arrays
+ * @returns {{}|*}
+ */
+function deepSortObject(unordered, sortArrays = false) {
+  if (!unordered || typeof unordered !== 'object') {
+    return unordered;
+  }
+
+  if (Array.isArray(unordered)) {
+    const newArr = unordered.map((item) => deepSortObject(item, sortArrays));
+    if (sortArrays) {
+      newArr.sort();
+    }
+    return newArr;
+  }
+
+  const ordered = Object.keys(unordered).sort().reduce(
+    (obj, key) => {
+      obj[key] = unordered[key];
+      return obj;
+    },
+    {}
+  );
+
+  Object.keys(ordered)
+    .forEach((key) => {
+      ordered[key] = deepSortObject(unordered[key], sortArrays);
+    });
+  return ordered;
+}
+
+/**
+ * Reorganise the object using JSON stringify,
+ * this inconsistently supports arrays though depending on Interpreter or ECMaScript version
+ * therefore I've also created an alternate solution.
+ * @param obj
+ * @returns {string}
+ */
+function stringifySort(obj) {
+  const keys = {};
+  JSON.stringify(obj, (key, value) => {
+    keys[key] = null;
+    return value;
+  });
+  return JSON.stringify(obj, Object.keys(keys).sort());
+};
 
 module.exports = router;
--- test/json.spec.js
+++ test/json.spec.js
@@ -3,32 +3,122 @@ const app = require('../src/app') // the express server
 const expect = require('chai').expect
 const fs = require('fs')
 
+
+// TODO - Work out why this logic didn't work.
+function assertionHelper(inputArray) {
+  return request(app)
+    .post('/json')
+    .send(inputArray)
+}
+
 describe('GET /json endpoint', () => {
 
-    it('tests manually generated example', () => {
-        const json = {
-            hello: 'world'
-        };
-        return request(app)
-            .post('/json')
-            .send(json)
-            .then((response) => {
-                expect(response.statusCode).to.equal(200)
-                expect(response.text).to.equal(JSON.stringify(json))
-            })
+    //Expect 1 result, 1 duplicate
+    const shallowArray = [
+      {
+        a: 1,
+        b: 1
+      },
+      {
+        b: 1,
+        a: 1
+      }
+    ]
+
+  //Expect 1 result, 1 duplicate
+  const deepArray = [
+    {
+      c: {
+        a: 1,
+        b: 1
+      }
+    },
+    {
+      c: {
+        b: 1,
+        a: 1
+      }
+    }
+  ]
+
+  //Expect 2 results, 1 duplicate
+  const simpleDuplicates = [
+    {
+      a: 1,
+      b: 1
+    },
+    {
+      a: 1,
+      b: 1
+    },
+    {
+      a: 2,
+      b: 1
+    }
+  ]
+
+    it('verifies a shallow sort', () => {
+      const expectedResponse = {
+        inputLength: 2,
+        uniqueLength: 1,
+        repeatedItems: 1
+      };
+      return request(app)
+        .post('/json')
+        .send(shallowArray).then((response) => {
+          expect(response.statusCode).to.equal(200)
+          expect(response.text).to.equal(JSON.stringify(expectedResponse))
+      })
     })
+
+    it('verifies a deep sort', () => {
+      const expectedResponse = {
+        inputLength: 2,
+        uniqueLength: 1,
+        repeatedItems: 1
+      };
+      return request(app)
+        .post('/json')
+        .send(deepArray).then((response) => {
+          expect(response.statusCode).to.equal(200)
+          expect(response.text).to.equal(JSON.stringify(expectedResponse))
+        })
+    })
+
+  it('verifies a simple sort', () => {
+    const expectedResponse = {
+      inputLength: 3,
+      uniqueLength: 2,
+      repeatedItems: 1
+    };
+    return request(app)
+      .post('/json')
+      .send(simpleDuplicates).then((response) => {
+        expect(response.statusCode).to.equal(200)
+        expect(response.text).to.equal(JSON.stringify(expectedResponse))
+      })
+  })
 
 
     it('tests nbs generated example', () => {
         const rawData = fs.readFileSync('assets/items.json');
         const json = JSON.parse(rawData);
+        const expectedResponse = {
+          inputLength: 1000,
+          uniqueLength: 975,
+          repeatedItems: 25
+        };
 
-        return request(app)
-            .post('/json')
-            .send(json)
-            .then((response) => {
-                expect(response.statusCode).to.equal(200)
-                expect(response.text).to.equal(JSON.stringify(json))
-            })
+      assertionHelper(json).then((response) => {
+        expect(response.statusCode).to.equal(200)
+        expect(response.text).to.equal(JSON.stringify(expectedResponse))
+      })
+
+      return request(app)
+        .post('/json')
+        .send(json).then((response) => {
+          expect(response.statusCode).to.equal(200)
+          expect(response.text).to.equal(JSON.stringify(expectedResponse))
+        })
     })
 })
